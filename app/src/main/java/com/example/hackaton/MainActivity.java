0525452df7fd0a9c07da9cb96de2fc406cb6e7a8
god-of-jesters <ryzovdima@gmail.com>
2023-04-09 21:43:42 +0300

    First

--- app/src/main/java/com/example/hackaton/MainActivity.java
+++ app/src/main/java/com/example/hackaton/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.hackaton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

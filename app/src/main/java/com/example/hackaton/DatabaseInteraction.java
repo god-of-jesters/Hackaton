43ff2e6248f9b9fce1338a750debe2dd4a95b0e1
god-of-jesters <ryzovdima@gmail.com>
2023-04-11 18:15:14 +0300

    Login add

---
+++ app/src/main/java/com/example/hackaton/DatabaseInteraction.java
@@ -0,0 +1,7 @@
+package com.example.hackaton;
+
+public class DatabaseInteraction{
+    public Boolean log(String name, String password){
+        return false;
+    }
+}
--- app/src/main/java/com/example/hackaton/Login.java
+++ app/src/main/java/com/example/hackaton/Login.java
@@ -6,14 +6,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
-import java.util.Map;
+public class Login extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
+    Button login;
+    EditText name;
+    EditText password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
